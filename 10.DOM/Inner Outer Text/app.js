c2fdca8455414882178944dacb28893400dbda81
Silvia <alumno.1360752@ies-azarquiel.es>
2022-11-18 08:16:30 +0100

    trabajando con DOM

--- 10.DOM/Inner Outer Text/app.js
+++ 10.DOM/Inner Outer Text/app.js
@@ -6,10 +6,10 @@ inner.innerHTML="hola a <b>todosss</b>"
 let outer=document.getElementById("outer")
 outer.outerHTML="<ul><li>A</li><li>B</li></<ul>"
 
-//modificamos contenido de un alemento, no distingue las entidades html
+//modificamos contenido de un elemento, no distingue las entidades html
 //Muestra lo del interior de la etiqueta, la etiqueta b no funcionaria
 let text= document.getElementById("text")
-text.innerText="<b>Adios a todos</b>"
+text.innerText="<b>Adios a todos</b>" //Se escribiria en texto plano, no funcionaria las etiquetas b
 
 //establezco un titulo
 inner.title ="mensaje al pasar el raton"
--- 10.DOM/Inner Outer Text/app2.js
+++ 10.DOM/Inner Outer Text/app2.js
@@ -1,23 +1,23 @@
 let special= document.getElementById("special")
 console.log(special)
-special.style.color="red"
+special.style.color="red" //Se añade el estilo inline
+special.classList.add("estilocss") //Se añade el estilo a traves del css asignando el nombre de la clase creada
 
 let lista_elementos = document.querySelectorAll("li")
 console.log(lista_elementos)
-let nodocalese=document.querySelector()
 
+//Formas de recorrer los elementos
 /* for (let i=0, i>lista_elementos.lenght;i++){
     lista_elementos[i].style.fontSize ="2rem"
 }*/
-
 
 /*for(const item of lista_elementos){
     item.style.fontSize="3rem"
 }*/
 
-
 lista_elementos.forEach(item=>{
-    item.style.fontSize="0.5rem"
+    item.style.fontSize="3rem"
+    special.classList.add("estilodecss")
 })
 
 
@@ -29,7 +29,7 @@ propiedades como nodeName
                 .firstChild/lastChild
                 .childNodes (#text lo entiende como sifuesen espacios en blanco)
                 for (let i = 0; i < nodeList.length; i++) {
-  text += nodeList[i].nodeName + "<br>";
+                text += nodeList[i].nodeName + "<br>";
 
                 .previousSiblings
                 appendChildeNode
